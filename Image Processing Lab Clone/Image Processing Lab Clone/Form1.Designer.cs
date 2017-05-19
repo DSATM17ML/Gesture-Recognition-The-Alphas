0abedd609a82cc460a34d3114f061e041814f533
Ranjith D <ranjithyadavblue@gmail.com>
2017-05-19 21:30:13 +0530

    Automatic Label Addition Implemented

--- Image Processing Lab Clone/Image Processing Lab Clone/Form1.Designer.cs
+++ Image Processing Lab Clone/Image Processing Lab Clone/Form1.Designer.cs
@@ -46,12 +46,10 @@
             this.resizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mergeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.binarizationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-<<<<<<< HEAD
             this.splitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.hogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resizeAndSaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sVMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-=======
->>>>>>> a50649d2cfc743a6122c5b316e9d364e9e508483
             this.morphologyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.reApplyToolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
@@ -86,19 +84,8 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-<<<<<<< HEAD
-            this.resizeOnlyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.singleFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.multipleFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-=======
-            this.sourceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.destinationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.bothToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.resizeAndSaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.featureExtractionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.hOGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
->>>>>>> a50649d2cfc743a6122c5b316e9d364e9e508483
+            this.multipleFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -113,11 +100,7 @@
             this.optionsToolStripMenuItem,
             this.morphologyToolStripMenuItem,
             this.edgeDetectionToolStripMenuItem,
-<<<<<<< HEAD
             this.hOEFToolStripMenuItem});
-=======
-            this.featureExtractionToolStripMenuItem});
->>>>>>> a50649d2cfc743a6122c5b316e9d364e9e508483
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
@@ -169,13 +152,10 @@
             this.resizeToolStripMenuItem,
             this.mergeToolStripMenuItem,
             this.binarizationToolStripMenuItem,
-<<<<<<< HEAD
             this.splitToolStripMenuItem,
             this.hogToolStripMenuItem,
+            this.resizeAndSaveToolStripMenuItem,
             this.sVMToolStripMenuItem});
-=======
-            this.copyToolStripMenuItem});
->>>>>>> a50649d2cfc743a6122c5b316e9d364e9e508483
             this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
             this.optionsToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
             this.optionsToolStripMenuItem.Text = "Options";
@@ -240,15 +220,6 @@
             // 
             // resizeToolStripMenuItem
             // 
-            this.resizeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-<<<<<<< HEAD
-            this.resizeOnlyToolStripMenuItem});
-=======
-            this.sourceToolStripMenuItem,
-            this.destinationToolStripMenuItem,
-            this.bothToolStripMenuItem,
-            this.resizeAndSaveToolStripMenuItem});
->>>>>>> a50649d2cfc743a6122c5b316e9d364e9e508483
             this.resizeToolStripMenuItem.Name = "resizeToolStripMenuItem";
             this.resizeToolStripMenuItem.Size = new System.Drawing.Size(209, 26);
             this.resizeToolStripMenuItem.Text = "Resize";
@@ -268,7 +239,6 @@
             this.binarizationToolStripMenuItem.Text = "Binarization";
             this.binarizationToolStripMenuItem.Click += new System.EventHandler(this.binarizationToolStripMenuItem_Click);
             // 
-<<<<<<< HEAD
             // splitToolStripMenuItem
             // 
             this.splitToolStripMenuItem.Name = "splitToolStripMenuItem";
@@ -283,6 +253,13 @@
             this.hogToolStripMenuItem.Text = "Hog";
             this.hogToolStripMenuItem.Click += new System.EventHandler(this.hogToolStripMenuItem_Click);
             // 
+            // resizeAndSaveToolStripMenuItem
+            // 
+            this.resizeAndSaveToolStripMenuItem.Name = "resizeAndSaveToolStripMenuItem";
+            this.resizeAndSaveToolStripMenuItem.Size = new System.Drawing.Size(209, 26);
+            this.resizeAndSaveToolStripMenuItem.Text = "Resize and save";
+            this.resizeAndSaveToolStripMenuItem.Click += new System.EventHandler(this.resizeAndSaveToolStripMenuItem_Click);
+            // 
             // sVMToolStripMenuItem
             // 
             this.sVMToolStripMenuItem.Name = "sVMToolStripMenuItem";
@@ -290,8 +267,6 @@
             this.sVMToolStripMenuItem.Text = "SVM";
             this.sVMToolStripMenuItem.Click += new System.EventHandler(this.sVMToolStripMenuItem_Click);
             // 
-=======
->>>>>>> a50649d2cfc743a6122c5b316e9d364e9e508483
             // morphologyToolStripMenuItem
             // 
             this.morphologyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -539,7 +514,7 @@
             // 
             this.hOEFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.singleFileToolStripMenuItem,
-            this.multipleFileToolStripMenuItem});
+            this.multipleFilesToolStripMenuItem});
             this.hOEFToolStripMenuItem.Name = "hOEFToolStripMenuItem";
             this.hOEFToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
             this.hOEFToolStripMenuItem.Text = "HOEF";
@@ -591,14 +566,6 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
             // 
-<<<<<<< HEAD
-            // resizeOnlyToolStripMenuItem
-            // 
-            this.resizeOnlyToolStripMenuItem.Name = "resizeOnlyToolStripMenuItem";
-            this.resizeOnlyToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
-            this.resizeOnlyToolStripMenuItem.Text = "Resize Only";
-            this.resizeOnlyToolStripMenuItem.Click += new System.EventHandler(this.resizeOnlyToolStripMenuItem_Click);
-            // 
             // singleFileToolStripMenuItem
             // 
             this.singleFileToolStripMenuItem.Name = "singleFileToolStripMenuItem";
@@ -606,63 +573,12 @@
             this.singleFileToolStripMenuItem.Text = "Single File";
             this.singleFileToolStripMenuItem.Click += new System.EventHandler(this.singleFileToolStripMenuItem_Click);
             // 
-            // multipleFileToolStripMenuItem
+            // multipleFilesToolStripMenuItem
             // 
-            this.multipleFileToolStripMenuItem.Name = "multipleFileToolStripMenuItem";
-            this.multipleFileToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
-            this.multipleFileToolStripMenuItem.Text = "Multiple File";
-            this.multipleFileToolStripMenuItem.Click += new System.EventHandler(this.multipleFileToolStripMenuItem_Click);
-=======
-            // sourceToolStripMenuItem
-            // 
-            this.sourceToolStripMenuItem.Name = "sourceToolStripMenuItem";
-            this.sourceToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
-            this.sourceToolStripMenuItem.Text = "Source";
-            this.sourceToolStripMenuItem.Click += new System.EventHandler(this.sourceToolStripMenuItem_Click);
-            // 
-            // destinationToolStripMenuItem
-            // 
-            this.destinationToolStripMenuItem.Name = "destinationToolStripMenuItem";
-            this.destinationToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
-            this.destinationToolStripMenuItem.Text = "Destination";
-            this.destinationToolStripMenuItem.Click += new System.EventHandler(this.destinationToolStripMenuItem_Click);
-            // 
-            // bothToolStripMenuItem
-            // 
-            this.bothToolStripMenuItem.Name = "bothToolStripMenuItem";
-            this.bothToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
-            this.bothToolStripMenuItem.Text = "Both";
-            this.bothToolStripMenuItem.Click += new System.EventHandler(this.bothToolStripMenuItem_Click);
-            // 
-            // copyToolStripMenuItem
-            // 
-            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
-            this.copyToolStripMenuItem.Size = new System.Drawing.Size(209, 26);
-            this.copyToolStripMenuItem.Text = "Copy";
-            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
-            // 
-            // resizeAndSaveToolStripMenuItem
-            // 
-            this.resizeAndSaveToolStripMenuItem.Name = "resizeAndSaveToolStripMenuItem";
-            this.resizeAndSaveToolStripMenuItem.Size = new System.Drawing.Size(190, 26);
-            this.resizeAndSaveToolStripMenuItem.Text = "Resize and Save";
-            this.resizeAndSaveToolStripMenuItem.Click += new System.EventHandler(this.resizeAndSaveToolStripMenuItem_Click);
-            // 
-            // featureExtractionToolStripMenuItem
-            // 
-            this.featureExtractionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.hOGToolStripMenuItem});
-            this.featureExtractionToolStripMenuItem.Name = "featureExtractionToolStripMenuItem";
-            this.featureExtractionToolStripMenuItem.Size = new System.Drawing.Size(140, 24);
-            this.featureExtractionToolStripMenuItem.Text = "Feature Extraction";
-            // 
-            // hOGToolStripMenuItem
-            // 
-            this.hOGToolStripMenuItem.Name = "hOGToolStripMenuItem";
-            this.hOGToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
-            this.hOGToolStripMenuItem.Text = "HOEF";
-            this.hOGToolStripMenuItem.Click += new System.EventHandler(this.hOGToolStripMenuItem_Click);
->>>>>>> a50649d2cfc743a6122c5b316e9d364e9e508483
+            this.multipleFilesToolStripMenuItem.Name = "multipleFilesToolStripMenuItem";
+            this.multipleFilesToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.multipleFilesToolStripMenuItem.Text = "Multiple Files";
+            this.multipleFilesToolStripMenuItem.Click += new System.EventHandler(this.multipleFilesToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -737,23 +653,13 @@
         private System.Windows.Forms.ToolStripMenuItem fillHolesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem reApplyToolStripMenuItem11;
         private System.Windows.Forms.ToolStripMenuItem binarizationToolStripMenuItem;
-<<<<<<< HEAD
         private System.Windows.Forms.ToolStripMenuItem splitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem hogToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resizeAndSaveToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem hOEFToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem sVMToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem resizeOnlyToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem singleFileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem multipleFileToolStripMenuItem;
-=======
-        private System.Windows.Forms.ToolStripMenuItem sourceToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem destinationToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem bothToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem resizeAndSaveToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem featureExtractionToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem hOGToolStripMenuItem;
->>>>>>> a50649d2cfc743a6122c5b316e9d364e9e508483
+        private System.Windows.Forms.ToolStripMenuItem multipleFilesToolStripMenuItem;
     }
 }
 
